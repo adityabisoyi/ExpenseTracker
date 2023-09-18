5ab7599107f12ac427d7da75574e89c511be9c9f
adityabisoyi <adibisoyi01@gmail.com>
2023-09-18 10:39:42 +0530

    Expense CRUD operations added

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,4 +1,5 @@
 const { getIncome, deleteIncome, addIncome } = require('../controller/Income');
+const { getExpense, deleteExpense, addExpense } = require('../controller/expense');
 
 
 const router = require('express').Router();
@@ -7,5 +8,8 @@ const router = require('express').Router();
 router.post('/add-income', addIncome)
       .get('/get-income', getIncome)
       .delete('/delete-income/:id', deleteIncome)
+      .post('/add-expense', addExpense)
+      .get('/get-expense', getExpense)
+      .delete('/delete-expense/:id', deleteExpense)
 
 module.exports = router

2a422cdde6a5a465f05d3eb417373278c093499f
adityabisoyi <adibisoyi01@gmail.com>
2023-09-19 20:22:16 +0530

    Item and button component added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { MainLayout } from "./styles/Layout";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Dashboard from "./components/Dashboard/Dashboard";
+import Item from "./components/Item/Item";
 
 function App() {
     const loadBody = () => {
@@ -11,9 +12,10 @@ function App() {
     return (
         <AppStyled className="App">
             <MainLayout>
-                <Sidebar/>
+                <Sidebar />
                 <main>
-                    <Dashboard/>
+                    <Dashboard />
+                    <Item />
                     {/* {loadBody()} */}
                 </main>
             </MainLayout>
@@ -23,17 +25,19 @@ function App() {
 
 const AppStyled = styled.div`
     height: 100vh;
-    background-color: #1C1E2D;
+    background-image: url(${props => props.bg});
     position: relative;
-
-    main {
+    main{
         flex: 1;
-        background: #151725;
-        border-radius: 20px;
+        background: rgba(252, 246, 249, 0.78);
+        border: 3px solid #FFFFFF;
+        backdrop-filter: blur(4.5px);
+        border-radius: 32px;
+        overflow-x: hidden;
         padding: 1em;
-        background: linear-gradient(145deg, #131521, #161928);
-        box-shadow: 20px 20px 60px #12141f,
-                    -20px -20px 60px #181a2b;
+        &::-webkit-scrollbar{
+            width: 0;
+        }
     }
 `
 
--- frontend/src/components/Item/Item.js
+++ frontend/src/components/Item/Item.js
@@ -1,11 +1,169 @@
 import React from 'react'
+import { money, freelance, stocks, users, bitcoin, card, piggy, book, food, medical, tv, groceries, clothing, travelling, circle, rupee, comment, calender, trash } from '../../utils/icons'
+import styled from 'styled-components'
+import { dateFormat } from '../../utils/dateFormat'
+import Button from '../Button/Button'
 
-function Item() {
-  return (
-    <div>
-      
-    </div>
-  )
+function Item({ id, title, amount, category, type, description, deleteItem, indicatorColor, date }) {
+
+    const incomeCategory = () => {
+        switch(category) {
+            case "salary" : 
+                return money
+
+            case "freelancing" : 
+                return freelance
+
+            case "investments" : 
+                return users
+
+            case "stocks" : 
+                return stocks;
+
+            case "cryto" : 
+                return bitcoin;
+
+            case "bank" : 
+                return card;
+
+            case "other" : 
+                return piggy;
+
+            default :
+                return ''
+        }
+    }
+
+    const expenseCategory = () => {
+        switch(category) {
+            case "eductaion" : 
+                return book;
+
+            case "food" : 
+                return food;
+
+            case "health" : 
+                return medical;
+
+            case "subscriptions" : 
+                return tv;
+
+            case "groceries" : 
+                return groceries;
+
+            case "clothing" : 
+                return clothing;
+
+            case "travelling" : 
+                return travelling;
+
+            case "other" : 
+                return circle;
+
+            default :
+                return ''
+        }
+    }
+
+
+    return (
+        <ItemStyled>
+            <div className="icon">
+                {type === 'expense' ? expenseCategory() : incomeCategory()}
+            </div>
+            <div className='content'>
+                <h5>{title}</h5>
+                <div className='inner-content'>
+                    <div className='text'>
+                        <p>{rupee} {amount}</p>
+                        <p>{calender} {dateFormat(date)}</p>
+                        <p>{comment} {description}</p> 
+                    </div>
+
+                    <div className="btn-con">
+                        <Button 
+                            icon={trash}
+                            bPad={'0.7rem'}
+                            bRad={'50%'}
+                            bg={'var(--primary-color'}
+                            color={'#fff'}
+                            iColor={'#fff'}
+                            hColor={'var(--color-green)'}
+                            onClick={() => deleteItem(id)}
+                        />
+                    </div>
+                </div>
+            </div>
+        </ItemStyled>
+    )
 }
+
+const ItemStyled = styled.div`
+     background: #FCF6F9;
+    border: 2px solid #FFFFFF;
+    box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+    border-radius: 20px;
+    padding: 1rem;
+    margin-bottom: 1rem;
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    width: 100%;
+    color: #222260;
+    .icon{
+        width: 60px;
+        height: 60px;
+        border-radius: 20px;
+        background: #F5F5F5;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border: 2px solid #FFFFFF;
+        i{
+            font-size: 2.6rem;
+        }
+    }
+
+    .content{
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        gap: .2rem;
+        h5{
+            font-size: 1.3rem;
+            padding-left: 2rem;
+            position: relative;
+            &::before{
+                content: '';
+                position: absolute;
+                left: 0;
+                top: 50%;
+                transform: translateY(-50%);
+                width: .8rem;
+                height: .8rem;
+                border-radius: 50%;
+                background: ${props => props.indicator};
+            }
+        }
+
+        .inner-content{
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            .text{
+                display: flex;
+                align-items: center;
+                gap: 1.5rem;
+                p{
+                    display: flex;
+                    align-items: center;
+                    gap: 0.5rem;
+                    color: var(--primary-color);
+                    opacity: 0.8;
+                }
+            }
+        }
+    }
+`
 
 export default Item
